aafec5f4ae4efa51836c513aa1f56080395d1b0d
EvanHsu21 <ehsu027@ucr.edu>
2021-05-09 21:59:22 -0700

    Completed part 2

--- turnin/ehsu027_lab8_part2.c
+++ turnin/ehsu027_lab8_part2.c
@@ -47,74 +47,91 @@ void PWM_off() {
 unsigned char i = 0;
 double note[8] = { 261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25 };
 
-enum SM_STATES { SM_Start, SM_Up, SM_Down, SM_Wait, SM_Off, SM_On } SM_State;
+enum SM_ONOFF { SM1_Start, SM1_On, SM1_Off, SM1_WaitOn, SM1_WaitOff } SM1_State;
+void Tick_OnOff() {
+	switch(SM1_State) {
+		case SM1_Start:
+			SM1_State = SM1_Off;
+		break;
+		case SM1_Off:
+			if ((PINA & 0x01) == 0x00) {
+				SM1_State = SM1_WaitOff;
+				PWM_on();
+				//set_PWM(note[i]);
+			}
+			else if ((PINA & 0x01) == 0x01) {
+				SM1_State = SM1_Off;
+			}
+		break;
+		case SM1_WaitOff:
+			if ((PINA & 0x01) == 0x01) {
+				SM1_State = SM1_On;
+			}
+			else {
+				SM1_State = SM1_WaitOff;
+			}
+		break;
+		case SM1_On:
+			if ((PINA & 0x01) == 0x00) {
+				SM1_State = SM1_WaitOn;
+				PWM_off();
+			}
+			else if ((PINA & 0x01) == 0x01) {
+				SM1_State = SM1_On;
+			}
+		break;
+		case SM1_WaitOn:
+			if ((PINA & 0x01) == 0x01) {
+				SM1_State = SM1_Off;
+			}
+			else {
+				SM1_State = SM1_WaitOn;
+			}
+		break;
+		default:
+		break;
+	}
+}
+
+
+enum SM_STATES { SM_Start, SM_Up, SM_Down, SM_Wait } SM_State;
 void Tick_Octave() {
 	switch(SM_State) {
 		case SM_Start:
-			if ((PINA & 0x01) == 0x00) {
-				SM_State = SM_On;
-				PWM_on();
-				set_PWM(note[i]);
-			}
-			else if ((PINA & 0x01) == 0x01) {
-				SM_State = SM_Start;
-			}
+			SM_State = SM_Wait;
+
 		break;
-		case SM_On:
-			if ((PINA & 0x01) == 0x00) {
-				SM_State = SM_On;
-			}
-			else if ((PINA & 0x01) == 0x01) {
-				SM_State = SM_Wait;
-			}
-		break;
-		case SM_Off:
-			if ((PINA & 0x01) == 0x00) {
-				SM_State = SM_Off;
-			}
-			else if ((PINA & 0x01) == 0x01) {
-				SM_State = SM_Start;
-			}
-		break;
+
 		case SM_Wait:
-			if ((PINA & 0x01) == 0x00) {
-				SM_State = SM_Off;
-				PWM_off();
-			}
-			else if ((PINA & 0x07) == 0x05) {
+			if ((PINA & 0x06) == 0x04) {
 				SM_State = SM_Up;
 				if (i < 7) {
 					++i;
 				}
 			}
-			else if ((PINA & 0x07) == 0x03) {
+			else if ((PINA & 0x06) == 0x02) {
 				SM_State = SM_Down;
 				if (i > 0) {
 					--i;
 				}
 			}
+			else if ((PINA & 0x06) == 0x06) {
+				SM_State = SM_Wait;
+			}
 		break;
 		case SM_Up:
-			if ((PINA & 0x01) == 0x00) {
-				SM_State = SM_Off;
-				PWM_off();
-			}
-			else if ((PINA & 0x07) == 0x05) {
+			if ((PINA & 0x06) == 0x04) {
 				SM_State = SM_Up;
 			}
-			else if ((PINA & 0x07) == 0x07) {
+			else if ((PINA & 0x06) == 0x06) {
 				SM_State = SM_Wait;
 			}
 		break;
 		case SM_Down:
-			if ((PINA & 0x01) == 0x00) {
-				SM_State = SM_Off;
-				PWM_off();
-			}
-			else if ((PINA & 0x07) == 0x03) {
+			if ((PINA & 0x06) == 0x02) {
 				SM_State = SM_Down;
 			}
-			else if ((PINA & 0x07) == 0x07) {
+			else if ((PINA & 0x06) == 0x06) {
 				SM_State = SM_Wait;
 			}
 		break;
@@ -125,11 +142,8 @@ void Tick_Octave() {
 	switch(SM_State) {
 		case SM_Start:
 		break;
-		case SM_Off:
-		break;
 		case SM_Wait:
-		break;
-		case SM_On:
+			set_PWM(note[i]);
 		break;
 		case SM_Up:
 			set_PWM(note[i]);
@@ -147,8 +161,10 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	SM_State = SM_Start;
+	SM1_State = SM1_Start;
     /* Insert your solution below */
 	while(1) {
+		Tick_OnOff();
 		Tick_Octave();
     }
     return 1;
